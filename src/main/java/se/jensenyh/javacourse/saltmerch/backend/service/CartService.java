5e96ba4da17f6b79e618e6237c3e1a8f6c04205e
Lukhasz <lukaskaarle@gmail.com>
2023-01-18 14:22:33 +0100

    Added a method in CartService.java

--- src/main/java/se/jensenyh/javacourse/saltmerch/backend/service/CartService.java
+++ src/main/java/se/jensenyh/javacourse/saltmerch/backend/service/CartService.java
@@ -1,7 +1,29 @@
 package se.jensenyh.javacourse.saltmerch.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import se.jensenyh.javacourse.saltmerch.backend.model.CartItem;
+import se.jensenyh.javacourse.saltmerch.backend.repository.CartRepository;
+
+import java.util.List;
+import java.util.Objects;
+
 public class CartService {
 
+    @Autowired
+    CartRepository cartRepository;
 
+    public List<CartItem> getCart() {
+        return cartRepository.selectAllItems();
+    }
+
+    public int addOrRemoveItemFromCart(String execute, CartItem cartItem) {
+        if (Objects.equals(execute, "add")) {
+            return cartRepository.insertOrIncrementItem(cartItem);
+        }
+        if (Objects.equals(execute, "remove")) {
+            return cartRepository.deleteOrDecrementItem(cartItem);
+        }
+        else return -1;
+    }
 
 }
--- src/main/java/se/jensenyh/javacourse/saltmerch/backend/service/ProductService.java
+++ src/main/java/se/jensenyh/javacourse/saltmerch/backend/service/ProductService.java
@@ -1,22 +1,11 @@
 package se.jensenyh.javacourse.saltmerch.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import se.jensenyh.javacourse.saltmerch.backend.model.CartItem;
-import se.jensenyh.javacourse.saltmerch.backend.repository.CartRepository;
-
-import java.util.List;
+import se.jensenyh.javacourse.saltmerch.backend.repository.ProductRepository;
 
 public class ProductService {
 
     @Autowired
-    CartRepository cartRepository;
-
-    public List<CartItem> getCart() {
-        return cartRepository.selectAllItems();
-    }
-
-    public int addOrRemoveItemFromCart(String execute, CartItem cartItem) {
-
-    }
+    ProductRepository productRepository;
 
 }
